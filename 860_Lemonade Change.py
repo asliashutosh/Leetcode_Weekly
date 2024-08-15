f20f33c0e8f17e3034b69395361212e9b575833f
Ashutosh Singh <asliashutosh@Ashutoshs-MacBook-Air.local>
2024-08-15 23:37:58 +0530

    weekly 860

---
+++ 860_Lemonade Change.py
@@ -0,0 +1,40 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+
+        count_of_fives = 0
+        count_of_tens = 0
+        count_of_twenty = 0
+
+        for price in bills:
+            if price == 5:
+                count_of_fives += 1
+
+            elif price == 10:
+                # Now check if you have change
+                if count_of_fives > 0:
+                    count_of_fives -= 1
+                    count_of_tens += 1
+                    
+                else:
+                    return False
+
+            elif price == 20:
+
+                if count_of_tens>0:
+                    count_of_tens -= 1
+
+                    if count_of_fives>0:
+                        count_of_fives -= 1
+                        count_of_twenty += 1
+
+                    else:
+                        return False
+
+                elif count_of_fives>=3:
+                    count_of_fives -= 3
+                    count_of_twenty += 1
+
+                else:
+                    return False
+
+        return True
